3f205a4e3c42917cdccddff48975e969b0b88e82
emilyrhee <emilyrhee99@gmail.com>
2023-08-17 18:19:01 -0400

    added note duration variables

--- rickroll.ino
+++ rickroll.ino
@@ -1,8 +1,13 @@
 #include "pitches.h"
 
-void playNote(int note, int duration) {
+int quarterNote = 1000;
+int dot(int note) {
+  return note / 2;
+}
+
+void playNote(int note, int duration, int rest = 0) {
     tone(8, note, duration);
-    delay(1000);
+    delay(1000 + rest);
     noTone(8);
 }
 
@@ -12,5 +17,6 @@ void setup() {
 void loop() {
     playNote(NOTE_C6, 1000);
     playNote(NOTE_D6, 1000);
-    
+    playNote(NOTE_G5, 1000);
+    playNote(NOTE_D6, 1000);
 }
