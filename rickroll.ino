fd2dc67bae10cc95e266649ad1be9cef654a709f
emilyrhee <emilyrhee99@gmail.com>
2023-08-21 16:25:49 -0400

    modified delay to have no addition

--- rickroll.ino
+++ rickroll.ino
@@ -7,9 +7,9 @@ int dot(int note) {
   return note / 2;
 }
 
-void playNote(int note, int duration, int rest = 0) {
+void playNote(int note, int duration, int rest = 1000) {
     tone(8, note, duration);
-    delay(1000 + rest);
+    delay(rest);
     noTone(8);
 }
 
@@ -22,7 +22,7 @@ void loop() {
     playNote(NOTE_G5, quarterNote);
     playNote(NOTE_D6, quarterNote);
     playNote(NOTE_E6, quarterNote);
-    playNote(NOTE_G6, eighthNote);
-    playNote(NOTE_F6, eighthNote);
-    playNote(NOTE_E6, eighthNote);
+    playNote(NOTE_G6, eighthNote, 500);
+    playNote(NOTE_F6, eighthNote, 500);
+    playNote(NOTE_E6, eighthNote, 500);
 }
