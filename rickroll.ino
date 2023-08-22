95c5f51458b74e9cfa50883cf20a8c244ab2a0c3
emilyrhee <emilyrhee99@gmail.com>
2023-08-22 17:38:34 -0400

    improved timing of song

--- rickroll.ino
+++ rickroll.ino
@@ -1,8 +1,8 @@
 #include "pitches.h"
 
 void playNote(int note, float duration) {
-    tone(8, note, duration * 4000);
-    delay(duration * 4000);
+    tone(8, note, duration * 2000);
+    delay(duration * 2000);
     noTone(8);
 }
 
@@ -10,12 +10,12 @@ void setup() {
 }
 
 void loop() {
-    playNote(NOTE_C6, 1/4.0);
-    playNote(NOTE_D6, 1/4.0);
+    playNote(NOTE_C6, 1/4.0 + 1/8.0);
+    playNote(NOTE_D6, 1/4.0 + 1/8.0);
     playNote(NOTE_G5, 1/4.0);
-    playNote(NOTE_D6, 1/4.0);
-    playNote(NOTE_E6, 1/4.0);
-    playNote(NOTE_G6, 1/8.0);
-    playNote(NOTE_F6, 1/8.0);
+    playNote(NOTE_D6, 1/4.0 + 1/8.0);
+    playNote(NOTE_E6, 1/4.0 + 1/8.0);
+    playNote(NOTE_G6, 1/16.0);
+    playNote(NOTE_F6, 1/16.0);
     playNote(NOTE_E6, 1/8.0);
 }
