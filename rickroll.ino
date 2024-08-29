393edfe046a58e84e3fbb83d7c02185763a27c9e
emilyrhee <emilyrhee99@gmail.com>
2024-08-28 21:33:37 -0400

    added more notes (hit that funky trumpet)

--- rickroll.ino
+++ rickroll.ino
@@ -8,10 +8,12 @@ void playNote(int note, float duration) {
     noTone(8);
 }
 
-void setup() {
+void rest(float duration) {
+    float milliseconds = duration * 2000;
+    delay(milliseconds);
 }
 
-void loop() {
+void setup() {
     playNote(NOTE_C6, 1/4.0 + 1/8.0);
     playNote(NOTE_D6, 1/4.0 + 1/8.0);
     playNote(NOTE_G5, 1/4.0);
@@ -20,4 +22,26 @@ void loop() {
     playNote(NOTE_G6, 1/16.0);
     playNote(NOTE_F6, 1/16.0);
     playNote(NOTE_E6, 1/8.0);
+    playNote(NOTE_C6, 1/4.0 + 1/8.0);
+    playNote(NOTE_D6, 1/4.0 + 1/8.0);
+    playNote(NOTE_G5, 1/2.0);
+    rest(1/2.0);
+    playNote(NOTE_G6, 1/16.0);
+    playNote(NOTE_G6, 1/16.0);
+    playNote(NOTE_A6, 1/16.0);
+    playNote(NOTE_C6, 1/16.0);
+    rest(1/16.0);
+    playNote(NOTE_C6, 1/16.0);
+    playNote(NOTE_C6, 1/4.0 + 1/8.0);
+    playNote(NOTE_D6, 1/4.0 + 1/8.0);
+    playNote(NOTE_G5, 1/4.0);
+    playNote(NOTE_D6, 1/4.0 + 1/8.0);
+    playNote(NOTE_E6, 1/4.0 + 1/8.0);
+    playNote(NOTE_G6, 1/16.0);
+    playNote(NOTE_F6, 1/16.0);
+    playNote(NOTE_E6, 1/8.0);
+}
+
+void loop() {
+
 }
